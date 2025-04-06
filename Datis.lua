d5a9018566cdacca66b230d7e17de21064405d27
Cocol1Hh <vsanimevideosnaruto@gmail.com>
2025-04-06 12:42:23 -0500

    Update Datis.lua

--- Datis.lua
+++ Datis.lua
@@ -6,7 +6,7 @@ local lplr = Players.LocalPlayer
 
 local jugadoresPermitidos = {
     ["iLordYamoshi666"] = true,
-    [""] = true
+    ["camilito10059"] = true
 }
 
 if not jugadoresPermitidos[lplr.Name] then
