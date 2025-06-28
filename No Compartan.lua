51a6cbf1957877a94e6912ca2602853db2efb56a
Cocol1Hh <vsanimevideosnaruto@gmail.com>
2025-06-28 13:12:12 -0500

    Actualizar No Compartan.lua

--- No Compartan.lua
+++ No Compartan.lua
@@ -1874,8 +1874,8 @@ task.spawn(function()
                 local count = tonumber(text:match("%((%d+)%)")) or 0
                 if specialUsers[lplr.Name] and count >= 1 then
                     game.ReplicatedStorage.Package.Events.reb:InvokeServer(9999)
-                elseif not specialUsers[lplr.Name] and count >= 10 then
-                    game.ReplicatedStorage.Package.Events.reb:InvokeServer(10)
+                elseif not specialUsers[lplr.Name] and count >= 4 then
+                    game.ReplicatedStorage.Package.Events.reb:InvokeServer(4)
                 end
             end
         end)
